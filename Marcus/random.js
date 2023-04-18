15c2caa07dc0619443b5a90150d4fccbb15288f8
mrxie68 <mrxie6@gmail.com>
2023-04-18 17:41:34 +0800

    Site updated: 2023-04-18 17:41:33

--- Marcus/random.js
+++ Marcus/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/03/17/Butterfly主题说明文档/","2023/04/17/HomeKit升级后ipad不能当作中枢的解决办法/","2023/03/16/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/03/17/Butterfly主题说明文档/","2023/03/16/hello-world/","2023/04/17/HomeKit升级后ipad不能当作中枢的解决办法/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
