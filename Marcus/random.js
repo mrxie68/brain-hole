b01c8a5d2a67858104ac0192f1870b783a4c121c
mrxie68 <mrxie6@gmail.com>
2023-05-24 16:30:57 +0800

    Site updated: 2023-05-24 16:30:57

---
+++ Marcus/random.js
@@ -0,0 +1,1 @@
+var posts=["2023/03/17/Butterfly/","2023/04/17/HomeKit/","2023/04/25/Macphotoshop/","2023/04/19/Windowslany/","2023/03/16/hello-world/","2023/05/18/goujian/","2023/05/18/goujianw/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
